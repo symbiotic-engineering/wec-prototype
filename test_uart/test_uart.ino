c4ca12d4c0acff377937c62fac5c8f8c48b97f81
Rebecca McCabe <chessgirl2400@gmail.com>
2022-12-10 15:37:45 -0500

    uart progress

---
+++ test_uart/test_uart.ino
@@ -0,0 +1,72 @@
+/*
+ Name:		VescUartSample.ino
+ Created:	9/26/2015 10:12:38 PM
+ Author:	AC
+*/
+
+// the setup function runs once when you press reset or power the board
+// To use VescUartControl stand alone you need to define a config.h file, that should contain the Serial or you have to comment the line
+// #include Config.h out in VescUart.h
+
+//Include libraries copied from VESC
+#include "VescUart.h"
+#include "datatypes.h"
+#include "Arduino.h"
+#include "HardwareSerial.h"
+
+//const int PB10 = 29;
+//const int PB11 = 30;
+
+
+//HardwareSerial Serial3(PB11,PB10);
+
+#define DEBUG
+unsigned long count;
+
+void setup() {
+	
+  //Serial3.setRx(PB11);
+  //Serial3.setTx(PB10);
+  pinMode(PC1,OUTPUT);
+
+	//Setup UART port
+	//Serial2.begin(115200);
+  #ifdef DEBUG
+	//SEtup debug port
+	Serial.begin(9600);
+	#endif
+}
+
+//struct bldcMeasure measuredValues;
+	
+// the loop function runs over and over again until power down or reset
+void loop() {
+	//int len=0;
+	//len = ReceiveUartMessage(message);
+	//if (len > 0)
+	//{
+	//	len = PackSendPayload(message, len);
+	//	len = 0;
+	//}
+	
+  /*
+	if (VescUartGetValue(measuredValues)) {
+		Serial.print("Loop: "); Serial.println(count++);
+		Serial.println(measuredValues);
+    //digitalWrite()
+     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);  
+                   // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);    
+	}
+	else
+	{
+		Serial.println("Failed to get data!");
+     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  
+	}
+  */
+	
+}
