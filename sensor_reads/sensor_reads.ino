efb2b041d800cbdc5cbc01269ee100b0ac38d1b2
Nelson Ooi <nelsonwmooi@gmail.com>
2023-03-25 14:01:34 -0400

    Update sensor_reads.ino

--- sensor_reads/sensor_reads.ino
+++ sensor_reads/sensor_reads.ino
@@ -7,11 +7,29 @@
 //#include "vesc_uart.h" <- OLD LIBRARY
 
 #include <VescUart.h>
+#include <ezButton.h>
+#include <TimerOne.h>
 
 // ---- Definitions -----
 
-/** Initiate VescUart class */
+/** Initialize VescUart class */
 VescUart UART;
+
+/* FSM Variables */
+/* Initialize FSM Global variables */
+int fault = 0;
+int bat_voltage = 0;
+int advance = 0;
+
+enum State {ROOT, PRECHARGE, HIGH_VOLTAGE, SWITCH_WAIT, TORQUING, SWITCH_AUTO};
+enum State current_state = ROOT;
+
+/* FSM Testing Config */
+ezButton FATAL_FAULT(2);
+ezButton HARD_FAULT(4);
+ezButton SOFT_FAULT(7);
+ezButton ADVANCE(8);
+ezButton BAT_VOLTAGE(12);
 
 // We are creating a struct (short for structure) called vesc_reading, to hold all the values
 // embedded in the VESC's output
@@ -240,8 +258,6 @@ float torque_sensor_read(){
 //   volatile bool advance;
 //   volatile bool e_stop;
 
-//   //advance = 
-
 
 
 
@@ -249,7 +265,135 @@ float torque_sensor_read(){
 
 //   return state_current
 // }
+/**
+ * @brief Checks flags to perform FSM state transitions.
+ * Will modify advance global variable.
+ */
+void state_machine(){ //TODO take in valid input for current_state transitions
+  switch(current_state){
+    case ROOT: // (ROOT->PRECHARGE) advance && !faults
+      if(advance == 1 && fault == 0/*advance && !faults*/){
+        // advance = 0;
+        current_state = PRECHARGE;
+        Serial.println(current_state);
+      }
+      break;
+    case PRECHARGE: // (PRECHARGE->HIGH_VOLTAGE) !faults && bat_voltage_is_high_enough
+      if(fault == 0 && bat_voltage == 1/*!faults && bat_voltage_is_high_enough*/){
+        current_state = HIGH_VOLTAGE;
+        Serial.println(current_state);
+      }
+      break;
+    case HIGH_VOLTAGE: // (HIGH_VOLTAGE->SWITCH_WAIT) advance && !faults
+      if(advance == 1 && fault == 0/*advance && !faults*/){
+        current_state = SWITCH_WAIT;
+        Serial.println(current_state);
+      }
+      break;
+    case SWITCH_WAIT: // (SWITCH_WAIT->TORQUING) advance && !faults
+      if(advance == 1 && fault == 0/*advance && !faults*/){
+        current_state = TORQUING;
+        Serial.println(current_state);
+      }
+      break;
+    case TORQUING: // (TORQUING->TORQUING) !faults (TORQUING->ROOT) fatal_fault (TORQUING->PRECHARGE_DONE) hard_fault (TORQUING->SWITCH_AUTO) soft_fault
+      if(fault == 3/*fatal_fault*/){
+        current_state = ROOT;
+        Serial.println(current_state);
+      }
+      else if(fault == 2/*hard_fault*/){
+        current_state = HIGH_VOLTAGE;
+        Serial.println(current_state);
+      }
+      else if(fault == 1/*soft_fault*/){
+        current_state = SWITCH_AUTO;
+        Serial.println(current_state);
+      }      
+      break;
+    case SWITCH_AUTO: // (SWITCH_AUTO->TORQUING)!faults
+      if(fault == 0/*!faults*/){
+        current_state = TORQUING;
+        Serial.println(current_state);
+      }
+      break;
+  }
+  advance = 0;
+}
 
+/**
+ * @brief Simulate FSM behavior using button inputs
+ * 
+ */
+void simulate_fsm_behavior(){
+  FATAL_FAULT.loop(); // MUST call the loop() function first
+  HARD_FAULT.loop(); // MUST call the loop() function first
+  SOFT_FAULT.loop(); // MUST call the loop() function first
+  ADVANCE.loop(); // MUST call the loop() function first
+  BAT_VOLTAGE.loop(); // MUST call the loop() function first
+
+  if (FATAL_FAULT.isPressed()){
+    fault = 3;
+    // Serial.println("Fatal Fault switch: OFF -> ON");
+    // Serial.println(fault);
+  }
+
+  if (FATAL_FAULT.isReleased()){
+    fault = 0;
+    // Serial.println("Fatal Fault switch: ON -> OFF");
+    // Serial.println(fault);
+  }
+
+  if (HARD_FAULT.isPressed()){
+    fault = 2;
+    // Serial.println("Hard Fault switch: OFF -> ON");
+    // Serial.println(fault);
+  }
+    
+
+  if (HARD_FAULT.isReleased()){
+    fault = 0;
+    // Serial.println("Hard Fault switch: ON -> OFF");
+    // Serial.println(fault);
+  }
+
+  if (SOFT_FAULT.isPressed()){
+    fault = 1;
+    // Serial.println("Soft Fault switch: OFF -> ON");
+    // Serial.println(fault);
+  }
+
+  if (SOFT_FAULT.isReleased()){
+    fault = 0;
+    // Serial.println("Soft Fault switch: ON -> OFF");
+    // Serial.println(fault);
+  }
+
+  //if (ADVANCE.isPressed())
+  //  Serial.println("Advance Button: OFF -> ON");
+
+  if (ADVANCE.isReleased()){
+    if (advance == 0){
+      advance = 1;
+    }
+    else {
+      advance = 0;
+    }
+    // Serial.println("Advance Button: ON -> OFF");
+    // Serial.println(advance);
+  }   
+
+  if (BAT_VOLTAGE.isPressed()){
+    bat_voltage = 1;
+    // Serial.println("Battery Voltage switch: OFF -> ON");
+    // Serial.println(bat_voltage);
+  }
+
+  if (BAT_VOLTAGE.isReleased()){
+    bat_voltage = 0;
+    // Serial.println("Battery Voltage switch: ON -> OFF");
+    // Serial.println(bat_voltage);
+  }
+}
 
 
 //---------------------------------------------------------------------------------------------
@@ -270,6 +414,17 @@ void setup() {
 
   /** Define which ports to use as UART */
   UART.setSerialPort(&Serial3);
+
+  /** FSM code integration */
+  // initialize Timer ISR to check for state update
+  Timer1.initialize(100000);    
+  Timer1.attachInterrupt(state_machine);
+
+  FATAL_FAULT.setDebounceTime(50); // set debounce time to 50 milliseconds
+  HARD_FAULT.setDebounceTime(50);
+  SOFT_FAULT.setDebounceTime(50);
+  ADVANCE.setDebounceTime(50);
+  BAT_VOLTAGE.setDebounceTime(50);
 }
 
 
@@ -285,8 +440,11 @@ void loop() {
   float torque_val = torque_sensor_read();
   struct vesc_reading vesc_val = vesc_read();
 
+  simulate_fsm_behavior();
   //check_faults();
   //run_state_machine();
 
   
 }
+
+
