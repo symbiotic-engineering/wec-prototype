4b14b94814e09ef225734f3dc34055f966ee7e3e
rebeccamccabe <chessgirl2400@gmail.com>
2024-06-11 11:01:22 -0400

    add control value changes from 5/25/23 wave tank testing

--- calcVoltage.ino
+++ calcVoltage.ino
@@ -1,9 +1,9 @@
 
   #define gearRatio 10.0    // RM5 gear ratio
-  #define kp 600           // controller stiffness             [N/m]
-  #define bp 300            // controller damping               [Ns/m]
-  #define k_b 5           // motor constant                   [Nm/Amp (?)]
-  #define r_pinion 0.01  // lead of the lead screw           [m]
+  #define kp 3000           // controller stiffness             [N/m]
+  #define bp 1500            // controller damping               [Ns/m]
+  #define k_b 11.5           // motor constant               [Amp/Nm]
+  #define r_eff 0.010/(2*3.14)  // effective radius of the lead screw = lead/2pi           [m]
   #define I_thresh 13   // current threshold                [Amp]
   float v ;
   float x ;
@@ -12,8 +12,8 @@
 float calcVoltage(float I, float w_motor, float w_wave, float theta, int RMtype) {
 
   if (RMtype = 3) {
-    v = w_motor * r_pinion;
-    x = theta * r_pinion;
+    v = w_motor * r_eff;
+    x = theta * r_eff;
   } else if (RMtype = 5) {
     v = w_motor * gearRatio;
     x = theta * gearRatio;
@@ -21,7 +21,7 @@ float calcVoltage(float I, float w_motor, float w_wave, float theta, int RMtype)
 
   float F_desired = kp*x + bp*v;
 
-  float I_desired = F_desired * r_pinion * k_b;
+  float I_desired = F_desired * r_eff * k_b;
 
   Serial.print("kp*x = ");
   Serial.println(kp*x);
