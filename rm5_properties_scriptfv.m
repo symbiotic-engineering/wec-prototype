c8a8424f82e0c41a3309f9ced152eca7a283774f
cpokigo <cpp43@cornell.edu>
2024-07-10 12:44:40 -0400

    still exist errors and k value doesn't make sense

--- rm5_properties_scriptfv.m
+++ rm5_properties_scriptfv.m
@@ -7,60 +7,65 @@ density_sheet = 1180;     % Density of material 2 (in kg/m^3) polyprene sheet
 gravity = 9.81;           % Acceleration due to gravity (in m/s^2)
 density_water = 1000;     % kg/m^3
 
-% Geometry and dimensions of the object %%%% these dimensions need to be changed (7/8/24)
-height = 0.18;                  % Width (in meters) %%% 0.17 m
-length = 0.38;                  % Length (in meters) %%% 0.36 m
-t_sheet = 0.003175*2;           % Thickness of sheet (in meters) Polypropylene Sheet 2 sheets being used currently on each side
-t_foam = 0.02;                  % Thickness of foam (in meters)
-foam_area = t_foam*length;      %   NDG added this to 1d it
-sheet_area = t_sheet*length*2;  %   NDG added this to 1d it
-gap = 0.02;                     % distance from shaft to start of flap
+% Geometry and dimensions of the submerged part of flap
+height = 0.17;                  % Width (in meters)
+length = 0.36;                  % Length (in meters)
+t_sheet = 0.00555625;           % Thickness of sheet (in meters)
+t_foam = 0.0254;                % Thickness of foam (in meters)
+foam_area = t_foam * length;    % Total foam area
+sheet_area = t_sheet * length * 2;  % Total sheet area (includes both sides)
+gap = 0.02;                     % Distance from shaft to start of flap
 
 % Calculate the initial mass 
-volume_foam = height*foam_area;
-volume_sheet = height*sheet_area;
-mass_foam = density_foam*volume_foam*1.27; %%%% where is this coming from? (7/8/24)
-mass_sheet = density_sheet*volume_sheet;
-mass_total = mass_foam+mass_sheet;
+volume_foam = height * foam_area;
+volume_sheet = height * sheet_area;
+mass_foam = density_foam * volume_foam * 1.27; %%%% where is this coming from? (7/8/24)
+mass_sheet = density_sheet * volume_sheet;
+mass_total = mass_foam + mass_sheet;
 disp(['mass is ' num2str(mass_total)])
 
 %% FINDING THE CENTER OF BUOYANCY
-centroid = height/2;
+centroid = height/2; % center of mass of submerged component
 
 % Calculate the center of buoyancy as the weighted average of layer centroids
 volume_total = volume_sheet + volume_foam;
 disp(['volume is ' num2str(volume_total)])
 
 % Sum the weighted centroids to find the center of buoyancy
-center_of_buoyancy = centroid+gap; %%%% this calculation assumes fully submerged (7/8/24)
+center_of_buoyancy = centroid+gap;
 
 % Display the center of buoyancy
 disp(['Center of Buoyancy: ' num2str(center_of_buoyancy)]);
 
 %NOTE THAT WE CARAE about Y
-buoyancy_force = density_water * volume_total * %%%% again assumes fully submerged (7/8/24)
+buoyancy_force = density_water * volume_total;
 
-%% CALCULATING THE CURRENT CENTER OF MASS
-center_of_mass = centroid+gap; %%%% this calculation assumes fully submerged and uniform mass distribution (7/8/24), Q-are we matching to desired COM value or calculating for actual? 
+%% DEFINING CENTER OF MASS
+from_submerged = 3.9/50;  % WEC-Sim scaled distance of COM from water line (meters)
+center_of_mass = gap + height - from_submerged; % height above axis of rotation
 
 % Display the center of mass
 disp(['Center of Mass (x, y, z): ' num2str(center_of_mass)]);
 
 %% K calc
 desired_k = 0.653;
-our_k = center_of_buoyancy * buoyancy_force - mass_total * gravity * center_of_mass
+our_k = center_of_buoyancy * buoyancy_force - mass_total * gravity * center_of_mass;
 disp(['Stiffness: ' num2str(our_k) ' ~ ' num2str(desired_k) '?'])
 
 %% FINDING MOMENT OF INERTIA
 
-% Assuming the object is rotated around its center of mass in the xy-plane
-I_foam = (1/12)*mass_foam*height^2;
-I_sheet = (1/12)*mass_sheet*height^2;
+% Assuming the object is rotated around the same axis without the gap
+I_foam = density_foam*width*(t_sheet^3*height/12 + t_sheet*height^3/3);
 
-I_foam_adjusted = I_foam + mass_foam*(centroid+gap)^2;
-I_sheet_adjusted = I_sheet + mass_sheet*(centroid+gap)^2;
+a = t_foam/2;      % starting coordinate of foam in thickness direction
+b = a + t_sheet;   % ending coordinate of foam in thickness direction
+I_sheet = density_sheet*width*(b^3*height + b*height^3 - a^3*height - a*height^3);
 
-I_total = I_foam_adjusted + I_sheet_adjusted;
+% Adjust for the gap between flap and axis
+I_foam_adjusted = I_foam + mass_foam*(gap)^2;
+I_sheet_adjusted = I_sheet + mass_sheet*(gap)^2;
+
+I_total = I_foam_adjusted + 2*I_sheet_adjusted;
 
 % Display the total moment of inertia
 disp(['Total Moment of Inertia ' num2str(I_total)])
@@ -71,7 +76,7 @@ disp(['Total Moment of Inertia ' num2str(I_total)])
 new_K = our_k+dK;
 I_strut = 3.22e-5;
 new_I = I_total+dI+I_strut;
-new_m =mass_total+dm
+new_m = mass_total+dm;
 % Display the changes in mass, moment of inertia, and stiffness
 disp(['Change in Mass: ' num2str(dm)]);
 disp(['Change in Moment of Inertia: ' num2str(dI)]);
